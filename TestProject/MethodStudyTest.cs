1671e898c8a95732a9bcdab3333fdfc4ff3366cc
kyungmin-kim-teacher <k1k1m1@naver.com>
2021-04-29 15:53:03 +0900

    수정

--- TestProject/MethodStudyTest.cs
+++ TestProject/MethodStudyTest.cs
@@ -42,8 +42,8 @@ namespace TestProject
         public void 절대값함수_테스트_음수2()
         {
             MethodStudy ms = new MethodStudy();
-            int rv = ms.myabs(-2);
-            Assert.AreEqual(2, rv);
+            int rv = ms.myabs(-3);
+            Assert.AreEqual(3, rv);
         }
 
         /**
@@ -71,7 +71,7 @@ namespace TestProject
          * 함수명 : circle
          * 매개변수 : 반지름(r)을 나타내는 정수형 변수 1개
          * 반환형 : 실수형(float)
-         * 함수내용 : 두 정수 중 큰 값을 구하는 함수 구현
+         * 함수내용 : 파이(3.14f) * r * r로 원의 넓이 구하는 함수 구현
          */
         [TestMethod]
         public void 원의넓이함수_테스트1()
