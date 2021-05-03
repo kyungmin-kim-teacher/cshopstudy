3fb2488413d4d699a6bef600553c34334999d282
kimkyungmin <k1k1m1@naver.com>
2021-05-04 06:28:08 +0900

    if test

--- Hello/IfStudy.cs
+++ Hello/IfStudy.cs
@@ -8,6 +8,8 @@ namespace FirstCoding
 {
     public class IfStudy
     {
+
+
         public string 게임벌칙(int 등수)
         {
             if (등수 == 1)
@@ -16,6 +18,63 @@ namespace FirstCoding
             } else
             {
                 return "설거지";
+            }
+        }
+
+        public string 내가만약(string 나)
+        {
+            if (나 == "시인")
+            {
+                return "노래";
+            } 
+            else
+            {
+                return "춤";
+            }
+        }
+
+        public string 시험결과(int 점수)
+        {
+            if (점수 >= 80)
+            {
+                return "상장";
+            } 
+            else
+            {
+                return "참가상";
+            }
+        }
+
+        public bool 군대가야하나(string 그)
+        {
+            if (그 == "대한민국 국민")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool 도시락줄까(int 학년, string 이름)
+        {
+            if (학년 == 3 && 이름 == "김철수")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool 기념품줄까(int 학년)
+        {
+            if (학년 == 2 || 학년 ==3)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
             }
         }
     }
--- TestProject/IfStudyTest.cs
+++ TestProject/IfStudyTest.cs
@@ -6,6 +6,44 @@ namespace TestProject
     [TestClass]
     public class IfStudyTest
     {
+        [TestMethod]
+        public void 내가만약_테스트()
+        {
+            IfStudy ifs = new IfStudy();
+            string 할일 = ifs.내가만약("시인");
+            Assert.AreEqual("노래", 할일);
+
+            할일 = ifs.내가만약("가수");
+            Assert.AreEqual("춤", 할일);
+        }
+
+        [TestMethod]
+        public void 시험결과_테스트()
+        {
+            IfStudy ifs = new IfStudy();
+            int 점수 = 80;
+            string 결과 = ifs.시험결과(점수);
+            Assert.AreEqual("상장", 결과);
+
+            점수 = 79;
+            결과 = ifs.시험결과(점수);
+            Assert.AreEqual("참가상", 결과);
+        }
+
+        [TestMethod]
+        public void 군대가야하나_테스트()
+        {
+            IfStudy ifs = new IfStudy();
+
+            string 그 = "대한민국 국민";
+            bool 결과 = ifs.군대가야하나(그);
+            Assert.AreEqual(true, 결과);
+
+            그 = "중국 국민";
+            결과 = ifs.군대가야하나(그);
+            Assert.AreEqual(false, 결과);
+        }
+
         /**
          * 1등은 TV를 보고, 나머지는 모두 설거지 하기 함수
          */
@@ -34,6 +72,34 @@ namespace TestProject
             int 등수 = 10;
             string 할일 = ifs.게임벌칙(등수);
             Assert.AreEqual("설거지", 할일);
+        }
+
+        [TestMethod]
+        public void 도시락줄까_테스트()
+        {
+            IfStudy ifs = new IfStudy();
+
+            int 학년 = 3;
+            string 이름 = "김철수";
+            bool 도시락 = ifs.도시락줄까(학년, 이름);
+            Assert.AreEqual(true, 도시락);
+
+            Assert.AreEqual(false, ifs.도시락줄까(2, "김철수"));
+            Assert.AreEqual(false, ifs.도시락줄까(3, "박철수"));
+            Assert.AreEqual(false, ifs.도시락줄까(2, "박철수"));
+        }
+
+        [TestMethod]
+        public void 기념품줄까_테스트()
+        {
+            IfStudy ifs = new IfStudy();
+
+            int 학년 = 3;
+            bool 기념품 = ifs.기념품줄까(학년);
+            Assert.AreEqual(true, 기념품);
+
+            Assert.AreEqual(true, ifs.기념품줄까(2));
+            Assert.AreEqual(false, ifs.기념품줄까(1));
         }
     }
 }
