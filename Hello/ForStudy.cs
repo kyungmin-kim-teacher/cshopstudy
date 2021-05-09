2e0c61a874edc0cb83b7d78c9245c7175543b3d3
kimkyungmin <k1k1m1@naver.com>
2021-05-09 22:54:08 +0900

    for

---
+++ Hello/ForStudy.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace FirstCoding
+{
+    public class ForStudy
+    {
+        public void entry()
+        {
+            별나무(4);
+        }
+
+        private void 별나무(int 줄수)
+        {
+            string 별나무 = "";
+
+            for (int 줄번호 = 1; 줄번호 <= 줄수; 줄번호++)
+            {
+                for (int 횟수 = 1; 횟수 <= 줄번호; 횟수++)
+                {
+                    별나무 += "*";
+                }
+                if (줄번호 < 줄수)
+                {
+                    별나무 += "\n";
+                }
+            }
+            Console.WriteLine(별나무);
+        }
+
+        public string 수판별(int 수)
+        {
+            if (수 < 10)
+            {
+                return "small";
+            } else
+            {
+                return "big";
+            }
+        }
+
+        private void 별나무2()
+        {
+            string 첫번째줄 = "*\n";
+            string 두번째줄 = "**\n";
+            string 세번째줄 = "***\n";
+            string 네번째줄 = "****\n";
+            String 별나무 = 첫번째줄 + 두번째줄 + 세번째줄 + 네번째줄;
+            Console.WriteLine(별나무);
+        }
+
+        public int 빼기(int v1, int v2)
+        {
+            if (v1 > v2)
+            {
+                return v1 - v2;
+            } 
+            else
+            {
+                return v2 - v1;
+            }
+        }
+
+        public bool 칠배수(int v)
+        {
+            if (v <= 0)
+            {
+                return false;
+            }
+            if (v % 7 == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+
+        }
+
+        private void 별(int 별개수)
+        {
+            String 별표시 = "";
+            for (int 횟수 = 1; 횟수 <= 별개수; 횟수 = 횟수 + 1)
+            {
+                별표시 += "*";
+            }
+            Console.WriteLine(별표시);
+        }
+
+        public string 공던지기(int v)
+        {
+            if(v >= 50 && v <= 60)
+            {
+                return "win";
+            } 
+            else
+            {
+                return "lose";
+            }
+        }
+
+        /*
+         * 입력 : 장수 3
+         * [식권] #1
+         * [식권] #2
+         * [식권] #3
+         * 
+         */
+        private void 식권주세요(int 장수)
+        {
+            Console.WriteLine("[식권] #" + 장수);
+        }
+    }
+}
---
+++ TestProject/ForStudyTest.cs
@@ -0,0 +1,72 @@
+﻿using FirstCoding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ForStudyTest
+    {
+        /**
+         * 10보다 작은 정수가 입력되면 "small"을 출력하고,
+         * 10보다 크거나 같은 수가 입력되면 "big"을 출력하세요.
+         */
+        [TestMethod]
+        public void 수판별_테스트()
+        {
+            ForStudy fs = new ForStudy();
+
+            Assert.AreEqual("small", fs.수판별(3));
+            Assert.AreEqual("big", fs.수판별(10));
+            Assert.AreEqual("big", fs.수판별(13));
+        }
+
+        /**
+         * 정수 두 개가 입력으로 들어오면
+         * '큰수 - 작은수'를 뺀 값을 출력하세요.
+         */
+        [TestMethod]
+        public void 빼기_테스트()
+        {
+            ForStudy fs = new ForStudy();
+
+            Assert.AreEqual(2, fs.빼기(5, 3));
+            Assert.AreEqual(7, fs.빼기(10, 3));
+            Assert.AreEqual(7, fs.빼기(3, 10));
+        }
+
+        /**
+         * 어떤 양의 정수가 입력되면 그 수가 7의 배수면 true 아니면 false를 출력하세요
+         */
+        [TestMethod]
+        public void 칠배수_테스트()
+        {
+            ForStudy fs = new ForStudy();
+
+            Assert.AreEqual(true, fs.칠배수(7));
+            Assert.AreEqual(false, fs.칠배수(0));
+            Assert.AreEqual(false, fs.칠배수(1));
+            Assert.AreEqual(true, fs.칠배수(56));
+            Assert.AreEqual(false, fs.칠배수(-7));
+        }
+
+        /**
+         * 던진 공의 위치기 50이상 60이하일때 win, 그 외에는 lose를 출력
+         */
+        [TestMethod]
+        public void 공던지기_테스트()
+        {
+            ForStudy fs = new ForStudy();
+
+            Assert.AreEqual("lose", fs.공던지기(49));
+            Assert.AreEqual("win", fs.공던지기(50));
+            Assert.AreEqual("win", fs.공던지기(55));
+            Assert.AreEqual("win", fs.공던지기(60));
+            Assert.AreEqual("lose", fs.공던지기(61));
+        }
+    }
+}
