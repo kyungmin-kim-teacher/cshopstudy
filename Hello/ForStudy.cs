858223426f9a03d1f184d3443a1e239b0cc663b7
kyungmin-kim-teacher <k1k1m1@naver.com>
2021-05-13 14:29:57 +0900

    for

--- Hello/ForStudy.cs
+++ Hello/ForStudy.cs
@@ -6,7 +6,128 @@ namespace FirstCoding
     {
         public void entry()
         {
-            별나무(4);
+            나머지();
+        }
+
+        private void 나머지()
+        {
+            for(int a = 5; a <= 8; a++)
+            {
+                Console.Write("{0} % 3 :: ", a);
+                switch (a % 3)
+                {
+                    case 1:
+                        Console.WriteLine("나머지는 1 입니다.");
+                        break;
+                    case 2:
+                        Console.WriteLine("나머지는 2 입니다.");
+                        break;
+                    default:
+                        Console.WriteLine("나머지는 0 입니다.");
+                        break;
+
+                }
+            }
+        }
+
+        private void 요일2()
+        {
+            string[] weekDays =
+                { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            foreach (string day in weekDays)
+            {
+                switch (day)
+                {
+                    case "Mon":
+                        Console.WriteLine("월요일");
+                        break;
+                    case "Tue":
+                        Console.WriteLine("화요일");
+                        break;
+                    case "Wed":
+                        Console.WriteLine("수요일");
+                        break;
+                    case "Thu":
+                        Console.WriteLine("목요일");
+                        break;
+                    case "Fri":
+                        Console.WriteLine("금요일");
+                        break;
+                    default :
+                        Console.WriteLine("주말");
+                        break;
+                }
+            }
+        }
+
+        private void 요일()
+        {
+            string[] weekDays =
+                { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            foreach (string day in weekDays)
+            {
+                if (day == "Mon")
+                    Console.WriteLine("월요일");
+                else if (day == "Tue")
+                    Console.WriteLine("화요일");
+                else if (day == "Wed")
+                    Console.WriteLine("수요일");
+                else if (day == "Thu")
+                    Console.WriteLine("목요일");
+                else if (day == "Fri")
+                    Console.WriteLine("금요일");
+                else 
+                    Console.WriteLine("주말");
+            }
+        }
+
+        private void 반복중단()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (2 - i < 0)
+                {
+                    break;
+                }
+                Console.WriteLine("2-{0}={1}", i, (2 - i));
+            }
+        }
+
+        private void 배열()
+        {
+            //선언 & 할당
+            int[] array = new int[5];
+            string[] stringArray = new string[6];
+
+            //초기화
+            int[] numArr = new int[] { 1, 3, 5, 7, 9 };
+            
+            int[] numArr2 = { 1, 3, 5, 7, 9 };
+            string[] weekDays2 = 
+                { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            //참조
+            string[] weekDays = new string[]
+                { "Sunday", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            int[] nums = { 1, 2, 3, 4 };
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        private void 구구단()
+        {
+            for(int i = 2; i <= 9; i++)
+            {
+                for(int j = 2; j <= 9; j++)
+                {
+                    Console.WriteLine("{0} X {1} = {2}", i, j, i * j);
+                }
+            }
         }
 
         private void 별나무(int 줄수)
