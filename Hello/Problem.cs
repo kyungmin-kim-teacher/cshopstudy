2fafd771347b717f176e4d4c3ec00d6aeef7fbe9
kyungmin-kim-teacher <k1k1m1@naver.com>
2021-05-14 09:52:19 +0900

    Problem

--- Hello/Problem.cs
+++ Hello/Problem.cs
@@ -10,78 +10,27 @@ namespace FirstCoding
     {
         public int findMax(int v1, int v2, int v3)
         {
-            int max;
-
-            if (v1 > v2)
-            {
-                max = v1;
-            } 
-            else
-            {
-                max = v2;
-            }
-
-            if (max > v3)
-            {
-                return max;
-            } 
-            else
-            {
-                return v3;
-            }
+            throw new NotImplementedException();
         }
 
         public bool isVowel(char v)
         {
-            if (v == 'a' || v == 'i' || v == 'u'||
-                v == 'e' || v == 'o')
-            {
-                return true;
-            }
-            else
-            { 
-                return false;
-            }
+            throw new NotImplementedException();
         }
 
         public string ordinalNum(int v)
         {
-            if (v == 1)
-            {
-                return "1st";
-            }
-            if (v == 2)
-            {
-                return "2nd";
-            }
-            if (v == 3)
-            {
-                return "3rd";
-            }
-            return v + "th";
+            throw new NotImplementedException();
         }
 
         public int sum(int v)
         {
-            int rv = 0;
-            for (int i = 1; i <= v; i++)
-            {
-                rv += i;
-            }
-
-            return rv;
+            throw new NotImplementedException();
         }
 
         public int evenSum(int v)
         {
-            int rv = 0;
-            for (int i = 1; i <= v; i++)
-            {
-                if (i % 2 == 0)
-                    rv += i;
-            }
-
-            return rv;
+            throw new NotImplementedException();
         }
     }
 }
